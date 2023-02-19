8b4671d80ed77685ae29470fd1a1fd9dec0e88ef
Yeshey <yesheysangpo@hotmail.com>
2023-02-19 01:07:16 +0000

    Patched to kinda work on nixOS

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 import platform
+import distro
 
 def main():
     base_path = os.path.dirname(os.path.abspath(__file__))
@@ -12,12 +13,17 @@ def main():
     if system == 'Windows':
         cli_path = os.path.join(base_path, 'lib', 'windows', 'playit-cli.exe')
     elif system == 'Linux':
-        cli_path = os.path.join(base_path, 'lib', 'linux', 'playit-cli')
+        if 'NixOS' in distro.name():
+            cli_path = 'steam-run ' + os.path.join(base_path, 'lib', 'linux', 'playit-cli')
+            #cli_path = ['steam-run', cli_path]
+        else:
+            cli_path = os.path.join(base_path, 'lib', 'linux', 'playit-cli')
     else:
         print(f"Unsupported system: {system}")
         return
 
-    subprocess.run([cli_path])
+    os.system(cli_path)
+    #subprocess.run(cli_path)
 
 if __name__ == '__main__':
     main()
--- shell.nix
+++ shell.nix
@@ -1,9 +1,12 @@
 # pinning nixpkgs in version 21.11 (https://github.com/NixOS/nixpkgs/releases/tag/21.11), done as explained here: https://nix.dev/tutorials/towards-reproducibility-pinning-nixpkgs
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/a7ecde854aee5c4c7cd6177f54a99d2c1ff28a31.tar.gz") {} }:
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/a7ecde854aee5c4c7cd6177f54a99d2c1ff28a31.tar.gz") {
+  config.allowUnfree = true; # for steam-run
+} }:
 
 let
   my-python = pkgs.python3;
   python-with-my-packages = my-python.withPackages (p: with p; [
+    distro # to see that we're on nixOS
     # pandas
     #beautifulsoup4
     #requests
@@ -16,6 +19,7 @@ pkgs.mkShell {
   buildInputs = [
     python-with-my-packages
     pkgs.cacert # to fix certificate issues (https://github.com/NixOS/nixpkgs/issues/66716#issuecomment-883399373)
+    pkgs.steam-run # I know this sucks, but making nix work sucks more..
     #pkgs.curl
     #pkgs.unzip
     #pkgs.tar
