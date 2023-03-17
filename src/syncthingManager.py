3522ec0dbf5c18793277f0def76c4cf3cc7e95f7
Yeshey <yesheysangpo@hotmail.com>
2023-03-17 00:54:07 +0000

    dot dot dot

--- src/syncthingManager.py
+++ src/syncthingManager.py
@@ -274,7 +274,11 @@ class SyncthingManager:
                 if response.status_code == 200:
                     return True
             except requests.exceptions.RequestException:
-                print(f"[DEBUG] no syncthing detected, trying {3-i} more")
+                if i == 0:
+                    print(f"[DEBUG] no syncthing detected")
+                else:
+                    print(".", end="")
+                    
             
             time.sleep(3)
 
